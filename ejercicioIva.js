0eaeab0f789bc0b54803acbfed958a06f247f5a7
Luismi <glmg69@gmail.com>
2021-06-09 13:02:52 +0200

    ejercicioIva v2

--- ejercicioIva.js
+++ ejercicioIva.js
@@ -1,12 +1,12 @@
-function calcularIva(importe){
-    const IVA = 0.21;
-    return importe * IVA;
+function calcularIva(importe, impuesto=21){
+    return importe * impuesto / 100;
 }
 
-function calcularImporteTotal(importe){
-    return importe + calcularIva(importe);
+function calcularImporteTotal(importe,impuesto){
+    return importe + calcularIva(importe,impuesto);
 }
 
 let importeBase = 100;
 let importeTotal = calcularImporteTotal(importeBase);
-console.log("El importe total de " + importeBase + " es " + importeTotal);
+// console.log("El importe total de " + importeBase + " es " + importeTotal);
+console.log(`El importe total de ${importeBase} es ${importeTotal}`);
